8eb749e1c04084684d8c528f73a2da5cba5335d5
Antheny Wells <anthenywells@Go-away.local>
2020-01-17 17:21:03 -0500

    top nav

---
+++ src/TopNav/TopNav.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./TopNav.scss";
+
+function TopNav() {
+  return (
+    <div className="App">
+      <div className="top-nav">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Resume</a>
+        <a href="#">Work</a>
+        <a href="#">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default TopNav;
