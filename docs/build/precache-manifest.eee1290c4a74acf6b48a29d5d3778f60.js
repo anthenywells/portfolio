9905e1d31aeb0a530054a78fbe53b70f11851368
Antheny Wells <anthenywells@Go-away.local>
2020-04-23 22:26:14 -0400

    updated build

---
+++ docs/build/precache-manifest.eee1290c4a74acf6b48a29d5d3778f60.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5efa85697508500b50b1be824b642dbd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79507deac6de29b8359d",
+    "url": "/static/css/main.282dfaa0.chunk.css"
+  },
+  {
+    "revision": "4ed265852262de47a8a8",
+    "url": "/static/js/2.784770fa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.784770fa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "79507deac6de29b8359d",
+    "url": "/static/js/main.0229534d.chunk.js"
+  },
+  {
+    "revision": "78ba31876c96c74f9d41",
+    "url": "/static/js/runtime-main.bd5702e1.js"
+  },
+  {
+    "revision": "dde055fffd36889dfc277fbc4def27b3",
+    "url": "/static/media/angular-todo.dde055ff.jpg"
+  },
+  {
+    "revision": "a31f32c19b21ebb55581255274aa957d",
+    "url": "/static/media/antheny_wells_resume.a31f32c1.pdf"
+  },
+  {
+    "revision": "bfe9aaef70dbab922a2aca27ac4a3374",
+    "url": "/static/media/events-app.bfe9aaef.jpg"
+  },
+  {
+    "revision": "e5a3468209131d013ba29c959bd8b34f",
+    "url": "/static/media/musuem-of-candy.e5a34682.jpg"
+  },
+  {
+    "revision": "c417611d1d2f711cd5af52b5cf3bee32",
+    "url": "/static/media/profile_pic.c417611d.jpg"
+  },
+  {
+    "revision": "bb3742174fd9f1b74bad4dc198159005",
+    "url": "/static/media/table-finder.bb374217.jpg"
+  }
+]);
